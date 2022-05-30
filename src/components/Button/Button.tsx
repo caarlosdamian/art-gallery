d0f9fe57a71a3d2a53d46dda61fee7a8d6ce03cd
carlos eduardo damian perez <dapecarlos@gmail.com>
2022-05-30 17:43:51 -0500

    finish desktop application

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,17 +2,32 @@ import React from "react";
 import ArrowRight from "../../assets/icon-arrow-right.svg";
 import ArrowLeft from "../../assets/icon-arrow-left.svg";
 import "./Button.scss";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export const Button = () => {
+  const { pathname } = useLocation();
   return (
-    <Link className="button__container" to="/location">
-      <button className="button__container-input">OUR LOCATION</button>
-      <img
-        className="button__container-button"
-        src={ArrowRight}
-        alt="ArrowRight"
-      />
-    </Link>
+    <>
+      {pathname !== "/location" ? (
+        <Link className="button__container" to="/location">
+          <button className="button__container-input">OUR LOCATION</button>
+          <img
+            className="button__container-button"
+            src={ArrowRight}
+            alt="ArrowRight"
+          />
+        </Link>
+      ) : (
+        <Link className="button__container absolute" to="/">
+          <img
+            className="button__container-button"
+            src={ArrowLeft}
+            alt="ArrowLeft"
+          />
+          <button className="button__container-input">BACK TO HOME</button>
+          
+        </Link>
+      )}
+    </>
   );
 };
--- src/pages/Location/Location.tsx
+++ src/pages/Location/Location.tsx
@@ -1,11 +1,34 @@
-import React from 'react'
-import { useLocation } from 'react-router-dom';
+import React from "react";
+import { useLocation } from "react-router-dom";
+import map from "../../assets/desktop/image-map.png";
+import { Button } from "../../components/Button/Button";
+import { Footer } from "../../sections/Footer/Footer";
+import './Location.scss'
 
 export const Location = () => {
-  const location = useLocation()
+  const location = useLocation();
   console.log(location);
   return (
-    <div>Location</div>
-    
-  )
-}
+    <div className="location__container">
+      <Button/>
+      <img src={map} alt="maps" className="location__image" />
+      <div className="middle__container">
+        <div className="location__left">
+          <h1 className="left__location-header">OUR LOCATION</h1>
+        </div>
+        <div className="location__right">
+          <h1 className="location__rght-header">99 King Street</h1>
+          <p className="location__text">Newport</p>
+          <p className="location__text"> RI 02840</p>
+         
+          <p className="location__text"> United States of America</p>
+   
+          <p className="location__text-bottom">
+          Our newly opened gallery is located near the Edward King House on 99 King Street, the Modern Art Gallery is free to all visitors and open seven days a week from 8am to 9pm.
+          </p>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
